a8c92ef1191d4b42e196213fd565dee6cd00c42a
Eric Blossom <blossom@archive.org>
2016-11-28 00:24:04 -0800

    Add clojure-mode and cider.  Fix mac keyapping

--- init.el
+++ init.el
@@ -7,14 +7,30 @@
         ("marmalade"   . "http://marmalade-repo.org/packages/")
 	))
 
-(defvar my-packages 
-  '(better-defaults ergoemacs-mode magit python-mode php-mode yaml-mode))
-
+(defvar my-packages
+  '(better-defaults
+    projectile
+    clojure-mode
+    cider
+    magit
+    python-mode
+    php-mode
+    yaml-mode))
 
 (package-initialize)
 (dolist (p my-packages)
   (when (not (package-installed-p p))
     (package-install p)))
+
+;;(when (memq window-system '(mac ns))
+;;  (exec-path-from-shell-initialize))
+
+;; When on macOS: command -> meta, option -> super
+;; This matches my normal expectation and assumes
+;; that the keyboard itself hasn't be reprogrammed
+(when (memq window-system '(mac ns))
+  (setq ns-command-modifier 'meta)
+  (setq ns-alternate-modifier 'super))
 
 (require 'better-defaults)
 
@@ -26,9 +42,9 @@
 (require 'yaml-mode)
 (add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
 
-(require 'php-mode)
-(add-to-list 'auto-mode-alist '("\\.php\\'" . php-mode))
-(add-to-list 'auto-mode-alist '("\\.inc\\'" . php-mode))
+;(require 'php-mode)
+;(add-to-list 'auto-mode-alist '("\\.php\\'" . php-mode))
+;(add-to-list 'auto-mode-alist '("\\.inc\\'" . php-mode))
 
 
 ;;(require 'ergoemacs-mode)
@@ -49,7 +65,10 @@
  ;; If there is more than one, they won't work right.
  '(ansi-color-faces-vector
    [default default default italic underline success warning error])
- '(custom-enabled-themes (quote (misterioso))))
+ '(custom-enabled-themes (quote (misterioso)))
+ '(package-selected-packages
+   (quote
+    (cider clojure-mode projectile yaml-mode undo-tree python-mode php-mode persistent-soft magit exec-path-from-shell ergoemacs-mode better-defaults))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
