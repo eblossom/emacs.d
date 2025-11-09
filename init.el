3d74f39278cf39565633ac083c0bb9b8afe10f8b
Eric Blossom <ericb@projecteagle.net>
2025-11-09 14:32:21 -0800

    Remove scala related config

--- init.el
+++ init.el
@@ -18,10 +18,6 @@
 ;; This also sets the load path.
 (package-initialize)
 
-;;; ------------------------------------------------------------------------
-;;; Init stuff for scala metals.  Added 11/25/2020
-;;; https://scalameta.org/metals/docs/editors/emacs.html
-
 ;; Install use-package if not already installed
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
@@ -36,67 +32,23 @@
       backup-directory-alist `((".*" . ,temporary-file-directory))
       auto-save-file-name-transforms `((".*" ,temporary-file-directory t)))
 
-;; Enable scala-mode for highlighting, indentation and motion commands
-(use-package scala-mode
-  :interpreter
-    ("scala" . scala-mode))
-
-;; Enable sbt mode for executing sbt commands
-'(use-package sbt-mode
-  :commands sbt-start sbt-command
-  :config
-  ;; WORKAROUND: https://github.com/ensime/emacs-sbt-mode/issues/31
-  ;; allows using SPACE when in the minibuffer
-  (substitute-key-definition
-   'minibuffer-complete-word
-   'self-insert-command
-   minibuffer-local-completion-map)
-   ;; sbt-supershell kills sbt-mode:  https://github.com/hvesalai/emacs-sbt-mode/issues/152
-   (setq sbt:program-options '("-Dsbt.supershell=false"))
-)
-
 ;; Enable nice rendering of diagnostics like compile errors.
 (use-package flycheck
   :init (global-flycheck-mode))
-
-'(use-package lsp-mode
-  ;; Optional - enable lsp-mode automatically in scala files
-  :hook  (scala-mode . lsp)
-         (lsp-mode . lsp-lens-mode)
-  :config (setq lsp-prefer-flymake nil))
-
-;; Add metals backend for lsp-mode
-(use-package lsp-metals
-  :config (setq lsp-metals-treeview-show-when-views-received t))
-
-;; Enable nice rendering of documentation on hover
-(use-package lsp-ui)
 
 ;; lsp-mode supports snippets, but in order for them to work you need to use yasnippet
 ;; If you don't want to use snippets set lsp-enable-snippet to nil in your lsp-mode settings
 ;;   to avoid odd behavior with snippets and indentation
 (use-package yasnippet)
 
-;; Add company-lsp backend for metals
-'(use-package company-lsp)
-
 ;; Use the Debug Adapter Protocol for running tests and debugging
 (use-package posframe
   ;; Posframe is a pop-up tool that must be manually installed for dap-mode
-  )
-(use-package dap-mode
-  :hook
-  (lsp-mode . dap-mode)
-  (lsp-mode . dap-ui-mode)
   )
 
 (use-package ws-butler
   :hook
   (prog-mode . ws-butler-mode))
-
-;;; End scala metals init
-;;; ------------------------------------------------------------------------
-
 
 ;; Download the ELPA archive description if needed.
 ;; This informs Emacs about the latest versions of all packages, and
@@ -136,9 +88,6 @@
     ;; a filterable list of possible commands in the minibuffer
     ;; http://www.emacswiki.org/emacs/Smex
     smex
-
-    ;; project navigation
-    projectile
 
     ;; colorful parenthesis matching
     rainbow-delimiters
